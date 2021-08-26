ff5dbe3d8a2f3a9e9820af68f4f092f99600d058
Augusto <augustinhobrito542@gmail.com>
2021-08-26 15:53:33 -0300

    cria o resources

--- app/controllers/contatos_controller.rb
+++ app/controllers/contatos_controller.rb
@@ -2,4 +2,7 @@ class ContatosController < ApplicationController
   def index
     @contatos = Contato.all
   end
+  def new 
+    @contato = Contato.new
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'contatos#index'
+  resources :contatos
 end
