2ff4c9ab2f74aea2430f54a9806c55e143ebf35c
Augusto <augustinhobrito542@gmail.com>
2021-08-27 20:31:30 -0300

    link

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,2 +1,13 @@
 class Contato < ApplicationRecord
+  def mensagem_whatsapp(url)
+    CGI.escape("gostaria de mais informações sobre este emprestimo, aqui esta o link do meu cadastro:#{url}")
+  end
+
+  def telefone_wpp
+    '555198075560'
+  end
+
+  def nome_cpf
+    "#{nome} #{cpf}"
+  end
 end
